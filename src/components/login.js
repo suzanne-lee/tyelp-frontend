d96b5af0ea9f78d85089ddea045fbdd45db018c4
jesscall <jess.callegaro@icloud.com>
2018-11-13 10:12:22 -0500

    start

--- src/components/login.js
+++ src/components/login.js
@@ -1,20 +1,74 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import {Redirect} from 'react-router-dom';
+// import '../styles/login.css';
 // import * as actions from '../store/actions/app.actions';
 // import Store from '../store/store';
 
 // This should be a container
 class Login extends Component {
-  render() {
+    constructor(){
+        super();
 
-    //Store.dispatch(actions.SET_TOKEN_ACTION('aKFtu5M'));
+        this.state = {
+            username: '',
+            password: '',
+            redirectToReferrer: false
+        };
 
-    return (
-        <div >
-          Login Page
-        </div>
-    );
-  }
+        this.login = this.login.bind(this);
+        this.onChange = this.onChange.bind(this);
+
+    }
+
+
+
+    login() {
+        if(this.state.username && this.state.password){
+            
+            /*PostData('login',this.state).then((result) => {
+                let responseJson = result;
+                if(responseJson.userData){
+                    sessionStorage.setItem('userData',JSON.stringify(responseJson));
+                    this.setState({redirectToReferrer: true});
+                }
+
+            });*/
+        }
+
+    }
+
+    onChange(e){
+        this.setState({[e.target.name]:e.target.value});
+    }
+
+
+
+
+    render() {
+
+        if (this.state.redirectToReferrer) {
+            return (<Redirect to={'/home'}/>)
+        }
+
+        if(sessionStorage.getItem('userData')){
+            return (<Redirect to={'/home'}/>)
+        }
+
+        return (
+            <div className="row" id="Body">
+                <div className="medium-5 columns left">
+                    <h4>Login</h4>
+                    <label>Username</label>
+                    <input type="text" name="username" placeholder="Username" onChange={this.onChange}/>
+                    <label>Password</label>
+                    <input type="password" name="password"  placeholder="Password" onChange={this.onChange}/>
+                    <input type="submit" className="button success" value="Login" onClick={this.login}/>
+                    <a href="/signup">Registration</a>
+                </div>
+            </div>
+        );
+    }
 }
 
 /*
